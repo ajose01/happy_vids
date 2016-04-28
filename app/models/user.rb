1656bbc295e993375413898cad225bf386bfe077
Angel Jose <angel@sensay.it>
2016-04-27 23:26:33 -0700

    Set up initial associations for user videos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-
+  has_many :users_video
+  has_many :videos, through: :users_video
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,4 @@
 class Video < ActiveRecord::Base
-  
+  has_many :users_video
+  has_many :users, through: :users_video
 end
