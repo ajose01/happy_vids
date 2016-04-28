6b22a5a3d0f08d0bf0665023bc3580e7f6e25976
Angel Jose <angel@sensay.it>
2016-04-28 00:15:56 -0700

    prepare index views with partials for rendering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @table_columns = User.column_names
   end
 
   def show
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,6 +2,7 @@ class VideosController < ApplicationController
 
   def index
     @videos = Video.all
+    @table_columns = Video.column_names
   end
 
   def show
