1914f53d6f43ad2c2d1bb07441a7424a9ec67f43
Angel Jose <angel@sensay.it>
2016-04-27 23:13:29 -0700

    First commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
 class User < ActiveRecord::Base
-  # How does a user relate to a video?
-  # You should be able to run the following:
-  #    User.last.videos
+
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,3 @@
 class Video < ActiveRecord::Base
-  # How does a video relate to a user?
-  # You should be able to run the following:
-  #    Video.last.users
+  
 end
